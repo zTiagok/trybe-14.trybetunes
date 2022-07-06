a84b7e29aab71653484b8e7de2bd8ba5e6968ab8
Tiago <ztiagok@gmail.com>
2022-07-06 16:21:19 -0500

    c10 - 5º Requisito Completo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,58 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      btnDisabled: true,
+      search: '',
+    };
+  }
+
+  searchState = (origin) => {
+    const minLength = 2;
+    this.setState({ search: origin.target.value });
+
+    if (origin.target.value.length >= 2) {
+      this.setState({ btnDisabled: false });
+    } else {
+      this.setState({ btnDisabled: true });
+    }
+  };
+
+  buttonEvent = (origin) => {
+    origin.preventDefault();
+    this.setState({ btnDisabled: true, search: '' });
+  }
+
   render() {
+    const { btnDisabled, search } = this.state;
+
     return (
       <>
         <Header />
         <div data-testid="page-search" id="content-search">
-          Search Page
+          <form id="search-form">
+            <input
+              type="text"
+              id="search-input"
+              placeholder="Artist Name"
+              data-testid="search-artist-input"
+              minLength="2"
+              onChange={ this.searchState }
+              value={ search }
+            />
+            <button
+              type="submit"
+              form="search-form"
+              data-testid="search-artist-button"
+              disabled={ btnDisabled }
+              onClick={ this.buttonEvent }
+            >
+              Search
+            </button>
+          </form>
         </div>
       </>
     );
